f739ea024f1df1295a37e2cc29b2ade1c374f040
SomaRe <shivatej82@gmail.com>
2025-03-07 21:45:20 -0500

    feat: Add environment variable support for debug mode in application

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import webview
 import logging
+import os
+from dotenv import load_dotenv
 from src.app import Application
+
+load_dotenv()
 
 def main():
     # Configure logging
@@ -10,8 +14,15 @@ def main():
     app = Application()
     app.initialize()
 
+    use_debug = os.environ.get('WEBVIEW_DEBUG', 'False').lower() == 'true'
+
     # Start the application
-    webview.start(debug=True)
+    if use_debug:
+        logger.debug('Starting in debug mode')
+        webview.start(debug=True)
+    else:
+        logger.debug('Starting in production mode')
+        webview.start(ssl=True)
     logger.debug('webview.start finished')
 
 if __name__ == '__main__':
